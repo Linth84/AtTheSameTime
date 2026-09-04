786098e57e3176ec1fa733aa81e7edfce5a8d2a1
Linth84 <villalbaed84@gmail.com>
2026-09-04 14:26:53 -0300

    Fix GitHub Pages routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 
 import HomePage from './pages/HomePage'
 import EventPage from './pages/EventPage'
 
 function App() {
   return (
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/e/:slug" element={<EventPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
