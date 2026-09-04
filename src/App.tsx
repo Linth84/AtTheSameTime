82f0f71a3e52d05ce6bd68628fd43035877334b4
Linth84 <villalbaed84@gmail.com>
2026-09-04 14:19:21 -0300

    workflows

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 import HomePage from './pages/HomePage'
 import EventPage from './pages/EventPage'
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/e/:slug" element={<EventPage />} />
